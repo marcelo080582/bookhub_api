ebaa39d1e9675d1391d2376d8a095fd64da99034
Marcelo Martins <marcelo080582@gmail.com>
2026-06-18 19:05:01 -0300

    feat: add authors query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -18,14 +18,10 @@ module Types
       ids.map { |id| context.schema.object_from_id(id, context) }
     end
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :authors, [Types::AuthorType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def authors
+      Author.all
     end
   end
 end
